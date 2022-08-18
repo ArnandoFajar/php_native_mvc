6650c3daff8ee7b3c450f4dd82c1709a99e8c218
arnandofajar <arnandofajar@gmail.com>
2022-08-18 17:04:46 +0700

    first meet

---
+++ app/config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('base_url', 'http://localhost/php_native_mvc/public');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'belajar_buku');
